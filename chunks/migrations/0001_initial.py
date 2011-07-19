234722a6001d31ea8c3b722e05859091573eb013
Joe Jasinski <jjasinski@imagescape.com>
2011-07-19 18:07:55 -0500

    added a description field

---
+++ chunks/migrations/0001_initial.py
@@ -0,0 +1,35 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding model 'Chunk'
+        db.create_table('chunks_chunk', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('key', self.gf('django.db.models.fields.CharField')(unique=True, max_length=255)),
+            ('content', self.gf('django.db.models.fields.TextField')(blank=True)),
+        ))
+        db.send_create_signal('chunks', ['Chunk'])
+
+
+    def backwards(self, orm):
+        
+        # Deleting model 'Chunk'
+        db.delete_table('chunks_chunk')
+
+
+    models = {
+        'chunks.chunk': {
+            'Meta': {'object_name': 'Chunk'},
+            'content': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'key': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'})
+        }
+    }
+
+    complete_apps = ['chunks']
