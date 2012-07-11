f2b499ae22f9e64a80ac9e373b58975958e3ddf2
firass <firass@gmail.com>
2012-07-11 15:59:42 -0700

    Updated the version to 0.2
    django-chunks-0.2.tar.gz on code.google.com still says version='0.1' so pip freeze gives the old version.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='django-chunks',
-      version='0.1',
+      version='0.2',
       description='Keyed blocks of content for use in your Django templates',
       author='Clint Ecker',
       author_email='me@clintecker.com',
