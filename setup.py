d22a51071e99d3d9f2a90c0361b658bbb211fe36
Clint Ecker <me@clintecker.com>
2012-07-15 09:50:02 -0500

    updating version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='django-chunks',
-      version='0.2',
+      version='0.3',
       description='Keyed blocks of content for use in your Django templates',
       author='Clint Ecker',
       author_email='me@clintecker.com',
