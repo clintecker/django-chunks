8aa72f39f800bf583504621d7cc133c1f6aea01c
konstantin.gabbasov <konstantin.gabbasov@redsolution.ru>
2015-04-17 18:04:50 +0500

    _Russian translation added.

--- chunks/models.py
+++ chunks/models.py
@@ -1,4 +1,6 @@
+# -*- coding: utf-8 -*-
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 class Chunk(models.Model):
     """
@@ -7,9 +9,13 @@ class Chunk(models.Model):
     any template with the use of a special template
     tag
     """
-    key = models.CharField(help_text="A unique name for this chunk of content", blank=False, max_length=255, unique=True)
-    content = models.TextField(blank=True)
-    description = models.TextField(blank=True, unique=False)
+    key = models.CharField( _('Key'), help_text=_("A unique name for this chunk of content"), blank=False, max_length=255, unique=True)
+    content = models.TextField( _('Content'),blank=True)
+    description = models.TextField( _('Description'),blank=True, unique=False)
 
     def __unicode__(self):
         return u"%s" % (self.key,)
+
+    class Meta:
+        verbose_name = _('Chunk')
+        verbose_name_plural = _('Chunks')
